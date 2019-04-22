cc8b700589b1d20eeb5c1723f0274e22ef2eeac7
Crisliu7 <Crisliu7@github.com>
2019-04-22 17:28:56 -0400

    feat: add mask loss and roi align function

--- models.py
+++ models.py
@@ -294,7 +294,129 @@ class SamePad2d(nn.Module):
     def __repr__(self):
         return self.__class__.__name__
         
-        
+def pyramid_roi_align(inputs, pool_size=[14, 14], image_shape=[416, 416, 3]):
+    """Implements ROI Pooling on multiple levels of the feature pyramid.
+
+    Params:
+    - pool_size: [height, width] of the output pooled regions. Usually [7, 7]
+    - image_shape: [height, width, channels]. Shape of input image in pixels
+
+    Inputs:
+    - boxes: [batch, num_boxes, (y1, x1, y2, x2)] in normalized
+             coordinates.
+    - Feature maps: List of feature maps from different levels of the pyramid.
+                    Each is [batch, channels, height, width]
+
+    Output:
+    Pooled regions in the shape: [num_boxes, channels, height, width].
+    The width and height are those specific in the pool_shape in the layer
+    constructor.
+    """
+
+    # Currently only supports batchsize 1
+    for i in range(len(inputs)):
+        inputs[i] = inputs[i].squeeze(0)
+
+    # Crop boxes [batch, num_boxes, (y1, x1, y2, x2)] in normalized coords
+    boxes = inputs[0]
+
+    # Feature Maps. List of feature maps from different level of the
+    # feature pyramid. Each is [batch, height, width, channels]
+    feature_maps = inputs[1:]
+
+    # Assign each ROI to a level in the pyramid based on the ROI area.
+    y1, x1, y2, x2 = boxes.chunk(4, dim=1)
+    h = y2 - y1
+    w = x2 - x1
+
+    # Equation 1 in the Feature Pyramid Networks paper. Account for
+    # the fact that our coordinates are normalized here.
+    # e.g. a 224x224 ROI (in pixels) maps to P4
+
+    image_area = torch.FloatTensor([float(image_shape[0] * image_shape[1])], requires_grad=False)
+    # image_area = torch.Tensor([float(image_shape[0] * image_shape[1])], requires_grad = False)
+
+    if boxes.is_cuda:
+        image_area = image_area.cuda()
+    roi_level = 3 + torch.log2(torch.sqrt(h * w) / (224.0 / torch.sqrt(image_area)))
+    roi_level = roi_level.round().int()
+    roi_level = roi_level.clamp(1, 3)
+
+    # Loop through levels and apply ROI pooling to each. P1 to P3.
+    pooled = []
+    box_to_level = []
+    for i, level in enumerate(range(1, 4)):
+        ix = roi_level == level
+        if not ix.any():
+            continue
+        ix = torch.nonzero(ix)[:, 0]
+        level_boxes = boxes[ix.data, :]
+
+        # Keep track of which box is mapped to which level
+        box_to_level.append(ix.data)
+
+        # Stop gradient propogation to ROI proposals
+        level_boxes = level_boxes.detach()
+
+        # Crop and Resize
+        # From Mask R-CNN paper: "We sample four regular locations, so
+        # that we can evaluate either max or average pooling. In fact,
+        # interpolating only a single value at each bin center (without
+        # pooling) is nearly as effective."
+        #
+        # Here we use the simplified approach of a single value per bin,
+        # which is how it's done in tf.crop_and_resize()
+        # Result: [batch * num_boxes, pool_height, pool_width, channels]
+        ind = torch.zeros(level_boxes.size()[0], requires_grad=False).int()
+        # ind = torch.zeros(level_boxes.size()[0], requires_grad = False).int()
+        if level_boxes.is_cuda:
+            ind = ind.cuda()
+        feature_maps[i] = feature_maps[i].unsqueeze(0)  # CropAndResizeFunction needs batch dimension
+        pooled_features = CropAndResizeFunction(pool_size, pool_size, 0)(feature_maps[i], level_boxes, ind)
+        pooled.append(pooled_features)
+
+    # Pack pooled features into one tensor
+    pooled = torch.cat(pooled, dim=0)
+
+    # Pack box_to_level mapping into one array and add another
+    # column representing the order of pooled boxes
+    box_to_level = torch.cat(box_to_level, dim=0)
+
+    # Rearrange pooled features to match the order of the original boxes
+    _, box_to_level = torch.sort(box_to_level)
+    pooled = pooled[box_to_level, :, :]
+
+    return pooled
+
+
+def compute_mrcnn_mask_loss(target_masks, target_class_ids, pred_masks):
+    """Mask binary cross-entropy loss for the masks head.
+
+    target_masks: [batch, num_rois, height, width].
+        A float32 tensor of values 0 or 1. Uses zero padding to fill array.
+    target_class_ids: [batch, num_rois]. Integer class IDs. Zero padded.
+    pred_masks: [batch, proposals, height, width, num_classes] float32 tensor
+                with values from 0 to 1.
+    """
+    if target_class_ids.size():
+        # Only positive ROIs contribute to the loss. And only
+        # the class specific mask of each ROI.
+        positive_ix = torch.nonzero(target_class_ids > 0)[:, 0]
+        positive_class_ids = target_class_ids[positive_ix.data].long()
+        indices = torch.stack((positive_ix, positive_class_ids), dim=1)
+
+        # Gather the masks (predicted and true) that contribute to loss
+        y_true = target_masks[indices[:, 0].data, :, :]
+        y_pred = pred_masks[indices[:, 0].data, indices[:, 1].data, :, :]
+
+        # Binary cross entropy
+        loss = F.binary_cross_entropy(y_pred, y_true)
+    else:
+        loss = torch.Tensor([0], requires_grad=False)
+        if target_class_ids.is_cuda:
+            loss = loss.cuda()
+
+    return loss       
 
 def get_yolo_layers(model):
     a = [module_def['type'] == 'yolo' for module_def in model.module_defs]
